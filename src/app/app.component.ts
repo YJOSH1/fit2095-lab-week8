f3c668668fa4d4116acf689990a7fb30ad4a481b
yjosh1 <84948881+yjosh1@users.noreply.github.com>
2022-09-13 16:19:39 +1000

    started lab task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+
+interface parcel {
+  id: string,
+  sender: string,
+  address: string,
+  weight: number,
+  cost: number,
+  fragile: boolean
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fit2095-lab-week8';
+  db: parcel[] = [];
+  inputSender: string = "";
+  inputAddress: string = "";
+  inputWeight: number = 0;
+  inputCost: number = 0;
+  inputFragile: boolean = false;  
+
+  addParcel() {
+    let newParcel: parcel = {
+      id: uuidv4(),
+      sender: this.inputSender,
+      address: this.inputAddress,
+      weight: this.inputWeight,
+      cost: this.inputCost,
+      fragile: this.inputFragile
+    };
+    this.db.push(newParcel);
+    console.log(this.db);
+  }
+
 }
